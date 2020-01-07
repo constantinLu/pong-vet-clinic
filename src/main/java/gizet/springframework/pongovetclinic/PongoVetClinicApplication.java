5c63f26f16bd93566f7bffafb20d6e6a1b55e7bf
constantin.lungu <constantin.lungu@fortech.ro>
2020-01-07 09:17:35 +0200

    [PongoVet] Initial Project added.

---
+++ src/main/java/gizet/springframework/pongovetclinic/PongoVetClinicApplication.java
@@ -0,0 +1,13 @@
+package gizet.springframework.pongovetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongoVetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PongoVetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gizet/springframework/pongovetclinic/PongoVetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package gizet.springframework.pongovetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PongoVetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
