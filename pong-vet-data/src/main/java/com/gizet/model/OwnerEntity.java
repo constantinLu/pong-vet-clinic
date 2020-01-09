0a21f4b24081e584a52834cc638c8f7b0b738ad0
constantin.lungu <constantin.lungu@fortech.ro>
2020-01-09 15:36:46 +0200

    [PongoVet] 7.24 - Create Vet Speciality Entity, Asociate to Vet Closes #28

--- pong-vet-data/src/main/java/com/gizet/model/OwnerEntity.java
+++ pong-vet-data/src/main/java/com/gizet/model/OwnerEntity.java
@@ -4,12 +4,45 @@ import java.util.Set;
 
 public class OwnerEntity extends PersonEntity {
 
-    public OwnerEntity(String firstName, String lastName, Set<PetEntity> pets) {
+    public OwnerEntity(String firstName, String lastName, String address, String city, String telephone, Set<PetEntity> pets) {
         super(firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
         this.pets = pets;
     }
 
+    private String address;
+
+    private String city;
+
+    private String telephone;
+
     private Set<PetEntity> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<PetEntity> getPets() {
         return pets;
