a46a53f74c55803b1d7524c9baa39e6a2ea05334
constantin.lungu <constantin.lungu@fortech.ro>
2020-01-09 12:48:33 +0200

    [PongoVet] 7.24 - Create Vet Speciality Entity, Asociate to Vet Closes #28

---
+++ pong-vet-data/src/main/java/com/gizet/model/SpecialityEntity.java
@@ -0,0 +1,14 @@
+package com.gizet.model;
+
+public class SpecialityEntity extends BaseEntity {
+
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- pong-vet-data/src/main/java/com/gizet/model/VetEntity.java
+++ pong-vet-data/src/main/java/com/gizet/model/VetEntity.java
@@ -1,8 +1,21 @@
 package com.gizet.model;
 
+import java.util.Set;
+
 public class VetEntity extends PersonEntity {
 
-    public VetEntity(String firstName, String lastName) {
+    public VetEntity(String firstName, String lastName, Set<SpecialityEntity> specialies) {
         super(firstName, lastName);
+        this.specialies = specialies;
+    }
+
+    private Set<SpecialityEntity> specialies;
+
+    public Set<SpecialityEntity> getSpecialies() {
+        return specialies;
+    }
+
+    public void setSpecialies(Set<SpecialityEntity> specialies) {
+        this.specialies = specialies;
     }
 }
