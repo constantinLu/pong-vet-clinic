6eb4d4f6bc59afdf397b3c991c1a9133cd66c8f9
gizet <lunguucatalin@gmail.com>
2020-01-10 16:39:10 +0200

    [PongoVet] Convert Vets to JPA Entities Closes #38

--- pong-vet-data/src/main/java/com/gizet/model/SpecialityEntity.java
+++ pong-vet-data/src/main/java/com/gizet/model/SpecialityEntity.java
@@ -1,8 +1,18 @@
 package com.gizet.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "Speciality")
 public class SpecialityEntity extends BaseEntity {
 
     private String type;
+
+    @ManyToMany
+    private VetEntity vet;
 
     public String getType() {
         return type;
--- pong-vet-data/src/main/java/com/gizet/model/VetEntity.java
+++ pong-vet-data/src/main/java/com/gizet/model/VetEntity.java
@@ -1,14 +1,25 @@
 package com.gizet.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity(name ="Veterinary")
 public class VetEntity extends PersonEntity {
 
     public VetEntity(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<SpecialityEntity> specialities = new HashSet<>();
 
     public Set<SpecialityEntity> getSpecialities() {
