c5407d8174278848aa40b0fe100181dd6a84572d
gizet <lunguucatalin@gmail.com>
2020-01-13 16:17:11 +0200

    [PongoVet] Junit5 Closes #4

--- pongo-vet-web/src/test/java/com/gizet/startapp/PongoVetServiceEntityClinicApplicationTests.java
+++ pongo-vet-web/src/test/java/com/gizet/startapp/PongoVetServiceEntityClinicApplicationTests.java
@@ -1,9 +1,11 @@
 package com.gizet.startapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PongoVetServiceEntityClinicApplicationTests {
 
